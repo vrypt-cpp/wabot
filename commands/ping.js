728bf3a2bf8d39b89a7951873e9927c352e77c29
Vrypt <sanvrypt@gmail.com>
2026-05-21 07:37:24 +0700

    update

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,27 @@ export default {
 
   async execute({ sock, msg, from }) {
     const start = Date.now();
-    await sock.sendMessage(from, { text: '🏓 Pong!' }, { quoted: msg });
+
+    const sent = await sock.sendMessage(
+      from,
+      {
+        text: '🏓 Pong!',
+      },
+      {
+        quoted: msg,
+      }
+    );
+
     const latency = Date.now() - start;
-    await sock.sendMessage(from, { text: `🏓 Pong! ${latency}ms` }, { quoted: msg });
+
+    await sock.sendMessage(
+      from,
+      {
+        text: `🏓 Pong! ${latency}ms`,
+      },
+      {
+        edit: sent.key,
+      }
+    );
   },
 };
