bf0925ea2714377c054d86c9975d979104410bb8
Vrypt <muhamad.rizki27483@smp.belajar.id>
2026-05-25 08:46:43 +0700

    Update uptime command description and properties

--- commands/uptime.js
+++ commands/uptime.js
@@ -2,7 +2,12 @@ import { formatUptime } from '../utils/format.js';
 
 export default {
   name: 'uptime',
-  description: 'Uptime bot',
+  description: 'Tampilkan sudah berapa lama bot berjalan',
+  category: 'info',
+  ownerOnly: false,
+  scope: 'all',
+  cooldown: 10,
+  hidden: false,
 
   async execute({ sock, msg, from }) {
     await sock.sendMessage(from, {
